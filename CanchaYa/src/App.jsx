337cbb8238ca6a5c8595153bd46ba579c244af68
LuigiMarconiFavini <luigimarconifavini@gmail.com>
2025-08-30 18:41:45 -0300

    Agrego componente NotFound con alerta pendiente

--- CanchaYa/src/App.jsx
+++ CanchaYa/src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
+import NotFound from './components/notFound/NotFound'
 
 function App() {
 
   return (
     <>
-
+      <NotFound /> {/* Falta implementar bien la lógica cuando hagamos hecho el login */}
     </>
   )
 }
