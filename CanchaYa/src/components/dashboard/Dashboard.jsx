4f9c9614734bd2af5b9cbf8093f9b7da726988f9
Francesco Dagostino <francescodagostino1889@gmail.com>
2025-08-31 19:08:31 -0300

    Correción en Dashboard

--- CanchaYa/src/components/dashboard/Dashboard.jsx
+++ CanchaYa/src/components/dashboard/Dashboard.jsx
@@ -3,12 +3,12 @@ import React from 'react'
 const Dashboard = () => {
   return (
     <>
-        <h1>CANCHAS-YA</h1>
-        <P>Encontra tu las mejores canchas cerca de donde estes!!</P>
+        <h1>CANCHAS - YA</h1>
+        <p>Encontra tu las mejores canchas cerca de donde estes!!</p>
 
-        <Form>
+        <form>
             <input type="select" name="deporte" placeholder='Ingresa el Deporte' />
-        </Form>
+        </form>
     </>
   )
 }
