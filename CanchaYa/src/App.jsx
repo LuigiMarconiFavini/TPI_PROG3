a73dd1583ddb34c702a31641e87783cfc772dd5f
Francesco Dagostino <francescodagostino1889@gmail.com>
2025-08-31 01:37:26 -0300

    Correccion dashboard

--- CanchaYa/src/App.jsx
+++ CanchaYa/src/App.jsx
@@ -1,13 +1,14 @@
 import './App.css'
 import Dashboard from './components/dashboard/Dashboard'
-import NotFound from './components/notFound/NotFound'
+
 
 function App() {
 
   return (
     <>
-      {<NotFound /> /* Falta implementar bien la lógica cuando hagamos hecho el login */}
+      
       <Dashboard/>
+      
     </>
   )
 }
