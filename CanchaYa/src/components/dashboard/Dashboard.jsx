d2b941ed5c7c3ff860855880e12ab85652016e77
danilomercado <mercadodanilo213@hotmail.com>
2025-09-04 22:55:25 -0300

    agregue mock con datos, card de canchas, y mejore searchForm

--- CanchaYa/src/components/dashboard/Dashboard.jsx
+++ CanchaYa/src/components/dashboard/Dashboard.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
-import SearchForm from '../searchForm/SearchForm'
+import React from "react";
+import SearchForm from "../searchForm/SearchForm";
 
 const Dashboard = () => {
   return (
     <>
-        <h1>CANCHAS-YA</h1>
-        <p>Encontra tu las mejores canchas cerca de donde estes!!</p>
-        <SearchForm/>
+      <div className="text-center my-8">
+        <h1 className="text-4xl font-bold mb-2">CANCHAS-YA</h1>
+        <p className="text-lg text-gray-600">
+          Encontra tu las mejores canchas cerca de donde estes!!
+        </p>
+        <SearchForm />
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
---
+++ CanchaYa/src/mocks/Mock.js
@@ -0,0 +1,84 @@
+export const deportes = [
+  { value: "futbol", label: "Fútbol" },
+  { value: "padel", label: "Pádel" },
+];
+
+export const tiposPorDeporte = {
+  futbol: [
+    { value: "f5", label: "Fútbol 5" },
+    { value: "f7", label: "Fútbol 7" },
+    { value: "f8", label: "Fútbol 8" },
+    { value: "f9", label: "Fútbol 9" },
+  ],
+  padel: [
+    { value: "cubiertas", label: "Cubiertas" },
+    { value: "panoramicas", label: "Panorámicas" },
+    { value: "cesped", label: "Césped sintético" },
+    { value: "cielo", label: "Cielo abierto" },
+  ],
+};
+
+export const horarios = [
+  "9:00hs",
+  "10:00hs",
+  "11:00hs",
+  "12:00hs",
+  "13:00hs",
+  "14:00hs",
+  "15:00hs",
+  "16:00hs",
+  "17:00hs",
+  "18:00hs",
+  "19:00hs",
+  "20:00hs",
+  "21:00hs",
+  "22:00hs",
+  "23:00hs",
+];
+
+export const canchas = [
+  {
+    id: 1,
+    nombre: "Fútbol 5 La Esquina",
+    deporte: "futbol",
+    tipo: "f5",
+    direccion: "Av. San Martín 1234",
+    precio: 5000,
+    horarios: ["18:00hs", "19:00hs", "20:00hs"],
+    imagen:
+      "https://img.a.transfermarkt.technology/portrait/header/553469-1731009174.jpg?lm=1",
+  },
+  {
+    id: 2,
+    nombre: "Complejo Golazo",
+    deporte: "futbol",
+    tipo: "f7",
+    direccion: "Bv. Oroño 2345",
+    precio: 7000,
+    horarios: ["17:00hs", "18:00hs", "19:00hs"],
+    imagen:
+      "https://rosariocentral.com/wp-content/uploads/2023/02/CAMPAZ_GRANDE.png",
+  },
+  {
+    id: 3,
+    nombre: "Padel Center Norte",
+    deporte: "padel",
+    tipo: "cubiertas",
+    direccion: "Calle Belgrano 456",
+    precio: 4000,
+    horarios: ["17:00hs", "18:00hs", "19:00hs"],
+    imagen:
+      "https://img.bundesliga.com/tachyon/sites/2/2019/07/che_epl_ampadu_close_1920.jpg?crop=0px,0px,1920px,1080px",
+  },
+  {
+    id: 4,
+    nombre: "Padel Club Sur",
+    deporte: "padel",
+    tipo: "cesped",
+    direccion: "Av. Pellegrini 789",
+    precio: 3500,
+    horarios: ["16:00hs", "17:00hs", "18:00hs"],
+    imagen:
+      "https://marcaenzona.com/news/uploads/images/image_750x_67056451efdef.jpg",
+  },
+];
