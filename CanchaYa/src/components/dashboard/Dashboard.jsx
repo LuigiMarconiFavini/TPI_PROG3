42d6cae8f07e710c92ef6682cd63c8ea285dc255
LuigiMarconiFavini <luigimarconifavini@gmail.com>
2025-09-05 22:29:42 -0300

    Agrego componente Navbar.jsx (funcionalidad pendiente)

--- CanchaYa/src/components/dashboard/Dashboard.jsx
+++ CanchaYa/src/components/dashboard/Dashboard.jsx
@@ -1,13 +1,15 @@
 import React from "react";
+import Navbar from "../navbar/Navbar";
 import SearchForm from "../searchForm/SearchForm";
 
 const Dashboard = () => {
   return (
     <>
+      <Navbar />
       <div className="text-center my-8">
-        <h1 className="text-4xl font-bold mb-2">CANCHAS-YA</h1>
+        <h1 className="text-4xl font-bold mb-2">CANCHA-YA</h1>
         <p className="text-lg text-gray-600">
-          Encontra tu las mejores canchas cerca de donde estes!!
+          ¡Encontrá las mejores canchas cerca de donde estés!
         </p>
         <SearchForm />
       </div>
@@ -16,3 +18,4 @@ const Dashboard = () => {
 };
 
 export default Dashboard;
+
