2195ae58fd02b097e50d3622cbe1913e654751fd
danilomercado <mercadodanilo213@hotmail.com>
2025-09-17 23:26:20 -0300

    agregue el back de las canchas

--- CanchaYa/src/components/searchForm/SearchForm.jsx
+++ CanchaYa/src/components/searchForm/SearchForm.jsx
@@ -102,12 +102,12 @@ const SearchForm = () => {
         {/* // aca se busca */}
         <div className="flex flex-col w-48">
           <label className="invisible mb-2">Buscar</label>
-            <button
-              type="submit"
-              className="border border-gray-600 rounded-lg px-3 py-2 shadow-sm bg-gray-800 text-white focus:outline-none focus:ring-2 focus:ring-blue-400"
-            >
+          <button
+            type="submit"
+            className="border border-gray-600 rounded-lg px-3 py-2 shadow-sm bg-gray-800 text-white focus:outline-none focus:ring-2 focus:ring-blue-400"
+          >
             Buscar Canchas
-        </button>
+          </button>
         </div>
       </form>
       {/* // aca muestra lo que busca */}
