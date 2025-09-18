81956f82e3a952c5f3d3440936ff2aa961583bae
Francesco Dagostino <francescodagostino1889@gmail.com>
2025-09-18 10:24:18 -0300

    correct UpdateRole

--- CanchaYa-Server/src/controllers/usersController.js
+++ CanchaYa-Server/src/controllers/usersController.js
@@ -1,41 +1,41 @@
 import { User } from "../Models/User.js";
 import bcrypt from "bcryptjs"
 
-// Registro
-export const registerUser = async (req, res) => {
-  try {
-    const { username, email, password } = req.body;
 
-    const exists = await User.findOne({ where: { email } });
-    if (exists) return res.status(400).json({ msg: "El email ya está registrado" });
+export const registerUser = async(req, res) => {
+    try {
+        const { username, email, password } = req.body;
 
-    const hashedPassword = await bcrypt.hash(password, 10);
+        const exists = await User.findOne({ where: { email } });
+        if (exists) return res.status(400).json({ msg: "El email ya está registrado" });
 
-    const newUser = await User.create({
-      username,
-      email,
-      password: hashedPassword
-    });
+        const hashedPassword = await bcrypt.hash(password, 10);
 
-    res.status(201).json({ msg: "Usuario registrado", user: newUser });
-  } catch (err) {
-    res.status(500).json({ msg: "Error al registrar", error: err.message });
-  }
+        const newUser = await User.create({
+            username,
+            email,
+            password: hashedPassword
+        });
+
+        res.status(201).json({ msg: "Usuario registrado", user: newUser });
+    } catch (err) {
+        res.status(500).json({ msg: "Error al registrar", error: err.message });
+    }
 };
 
-// Login
-export const loginUser = async (req, res) => {
-  try {
-    const { email, password } = req.body;
 
-    const user = await User.findOne({ where: { email } });
-    if (!user) return res.status(400).json({ msg: "Usuario no encontrado" });
+export const loginUser = async(req, res) => {
+    try {
+        const { email, password } = req.body;
 
-    const validPassword = await bcrypt.compare(password, user.password);
-    if (!validPassword) return res.status(400).json({ msg: "Contraseña incorrecta" });
+        const user = await User.findOne({ where: { email } });
+        if (!user) return res.status(400).json({ msg: "Usuario no encontrado" });
 
-    res.json({ msg: "Login exitoso", user });
-  } catch (err) {
-    res.status(500).json({ msg: "Error en login", error: err.message });
-  }
+        const validPassword = await bcrypt.compare(password, user.password);
+        if (!validPassword) return res.status(400).json({ msg: "Contraseña incorrecta" });
+
+        res.json({ msg: "Login exitoso", user });
+    } catch (err) {
+        res.status(500).json({ msg: "Error en login", error: err.message });
+    }
 };
